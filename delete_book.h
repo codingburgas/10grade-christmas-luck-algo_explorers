0ff83a30bfa545a419735e4bb94404df41cf7c1c
Yoana Georgieva <YTGeorgieva22@codingburgas.bg>
2025-01-05 16:05:42 +0200

    Display book details

--- delete_book.h
+++ delete_book.h
@@ -6,3 +6,13 @@
 
 using namespace std;
 
+void deleteBook(int num) {
+    int number = findBookNumberByNum(num);
+    if (number != -1) {
+        books.erase(books.begin() + number);
+        cout << "Book deleted." << endl;
+    }
+    else {
+        cout << "Book not found." << endl;
+    }
+}
